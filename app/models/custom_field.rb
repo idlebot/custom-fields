69ceeb0c484e42462822665e0c50281a881d3d58
idlebot <idlebot@robotreasons.com>
2016-07-20 20:11:09 -0300

    Implement Single Table Inheritance for CustomField

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,10 +1,21 @@
 class CustomField < ActiveRecord::Base
 
   belongs_to :user
-  has_many :drop_down_values, dependent: :destroy
 
   validates :field_name,
     presence: true,
     length: { minimum: 2, maximum: 50 }
 
+end
+
+class TextCustomField < CustomField
+
+end
+
+class TextAreaCustomField < CustomField
+
+end
+
+class DropDownCustomField < CustomField
+  has_many :drop_down_values, dependent: :destroy
 end
