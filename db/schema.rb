f9e4a428256f42bda0e979ca114225bd38f770cf
idlebot <idlebot@robotreasons.com>
2016-07-20 00:29:46 -0300

    Add contacts table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719190010) do
+ActiveRecord::Schema.define(version: 20160720030705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20160719190010) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "contacts", "users"
 end
